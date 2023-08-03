831d2fddc12afd3d443618bdf41b1e7e1b0e7f26
Yenuli Minsandi Senevirathna <IT21381768@my.sliit.lk>
2023-08-03 08:24:48 +0530

    commit

---
+++ client/src/Signup.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Signup() {
+    return (
+        <div>
+            <h1>Register</h1>
+            <div className="mb-3">
+                <label htmlFor="email">
+                    <strong>Name</strong>
+                </label>
+                <input type="text"
+                    className="form-control"
+                    autoComplete="off"
+                    name="name"
+                    placeholder="Enter your name" />
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="email">
+                    <strong>Email</strong>
+                </label>
+                <input type="email"
+                    className="form-control"
+                    autoComplete="off"
+                    name="email"
+                    placeholder="Enter your email" />
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="email">
+                    <strong>Password</strong>
+                </label>
+                <input type="password"
+                    className="form-control"
+                    autoComplete="off"
+                    name="password"
+                    placeholder="Enter your password" />
+            </div>
+            <button type="submit" className="btn btn-primary">
+                Register
+            </button>
+            <p>Already have a account</p>
+            <Link to="/login" type="submit" className="btn btn-primary">
+                Login
+            </Link>
+        </div>
+    )
+}
+
+export default Signup
